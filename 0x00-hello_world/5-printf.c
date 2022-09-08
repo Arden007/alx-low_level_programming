562eaf17dcdafa53fe41b4da7601b329242487c3
Arden007 <cloveryuno664@gmail.com>
2022-09-08 20:53:54 +0200

    Print out something using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char phrase[] = "\"with proper grammar, but the outcome is a piece of art,";
+
+printf(phrase);
+return (0);
+}
