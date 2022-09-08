8059cfc3b441158be44aedab48177609f8df0da8
Arden007 <cloveryuno664@gmail.com>
2022-09-08 18:19:15 +0200

    Print out something

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-    char string[] = "/Programming is like building a multilingual puzzle";
-    puts(string);
+    char sentence[] = "/Programming is like building a multilingual puzzle";
+    puts(sentence);
     return (0);
 }
