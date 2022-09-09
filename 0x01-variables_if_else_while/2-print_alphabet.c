46ca030bef7b9c5b07ae25e53e7c4ede11a14b78
Arden007 <cloveryuno664@gmail.com>
2022-09-09 13:35:42 +0200

    Well stubbornness paid off , now it is just betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-    char c = 'a';
+char c = 'a';
 
-    for (; c <= 'z'; c++)
-        putchar(c);
-    putchar('\n');
+for (; c <= 'z'; c++)
+    putchar(c);
+putchar('\n');
 
-    return (0);
+return (0);
 }
