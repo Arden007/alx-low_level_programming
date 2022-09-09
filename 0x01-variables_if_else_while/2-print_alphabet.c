765907dd87540706680bcb6c61dbe6673d2163ed
Arden007 <cloveryuno664@gmail.com>
2022-09-09 13:20:20 +0200

    if indentation fails again lets move on

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
+
 #include <stdio.h>
-#include <stdlib.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char c;
 
-for (c = 'a'; c <= 'z'; c++)
-    putchar(c);
-    putchar("\n");
-return(0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+
+return (0);
 }
