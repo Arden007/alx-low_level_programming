469975b1327f6c15933e0093291a5612ef6762ad
Arden007 <cloveryuno664@gmail.com>
2022-09-13 17:36:13 +0200

    checks if c is lower and returns 1 if true and 0 if false, betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * islower - checks if c is lower and returns 1 if true and 0 if false
+ * _islower - checks if c is lower and
+ * returns 1 if true and 0 if false
  */
 
 int _islower(int c)
 {
-if( c >= 97 &&  c <= 122 )
+if (c >= 97 &&  c <= 122)
 return (1);
 return (0);
 }
