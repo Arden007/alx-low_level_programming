4ed993f6ad7df31b9c1a0386291bc78c90b5c0a6
Arden007 <cloveryuno664@gmail.com>
2022-09-13 17:16:55 +0200

    _x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _iprint_alphabet_x10 - prints the alaphabet x10
+ * print_alphabet_x10 - prints the alaphabet x10
  */
 
 void print_alphabet_x10(void)
 {
 int tens, c;
+
 for ( tens = 0; tens <= 9 ; tens++){
 for ( c = 'a'; c <= 'z' ; c++)
 {
 putchar(c);
 putchar('\n');
 }
-return (0);
 }
