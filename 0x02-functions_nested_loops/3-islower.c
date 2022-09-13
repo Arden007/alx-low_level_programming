de3a765b843b921ba53e57093c8dd5ce276fce01
Arden007 <cloveryuno664@gmail.com>
2022-09-13 17:44:37 +0200

    checks if c is lower and returns 1 if true and 0 if false, betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,9 @@
 /**
  * _islower - function to check if
  * character is lowercase
- * @c: The character in ASCII code
- * returns 1 if 'c is lowercase
- *  and 0 otherwise
+ * @c: checks input of function
+ * Return: returns 1 if `c` is lowercase
+ * otherwise always 0 (Success)
  */
 
 int _islower(int c)
