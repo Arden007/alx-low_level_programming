b97785008273475fe2738dd4356cbf5b6e614d85
Arden007 <cloveryuno664@gmail.com>
2022-09-13 16:31:18 +0200

    _Print alphabet, check spelling

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
 char c;
     
-for ( c='a' ; c <= 'a' ; c++ )
+for ( c='a' ; c <= 'z' ; c++ )
 putchar(c);
 putchar('\n');
 }
