65f23c938609028e97dd3ccdb0a917cab6bb6ed9
Arden007 <cloveryuno664@gmail.com>
2022-09-13 17:52:49 +0200

    checks if c is a letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,12 @@
  * _isalpha - checks if character is a letter
  * both lowercase or uppercase
  * @c: takes input from other functions.
+ * Return: 1 for letters. Returns 0 otherwise
  */
 
 int _isalpha(int c)
 {
-if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
+if ((c >= 97 && c <= 122 ) || (c >= 65 && c >= 90))
 return (1);
 return (0);
 }
