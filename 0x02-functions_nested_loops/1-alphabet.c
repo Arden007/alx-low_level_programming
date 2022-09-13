f029b1e08caff510a1b81d873560fbf59443df0f
Arden007 <cloveryuno664@gmail.com>
2022-09-13 16:26:16 +0200

    _Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabt(void)
+{
+char c;
+    
+for ( c='a' ; c <= 'a' ; c++ )
+putchar(c);
+putchar("\n");
+return (0);
+}
