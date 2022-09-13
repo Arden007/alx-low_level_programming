fa90c4a3ee3750c63cfb34b0bb48477fa7cd7978
Arden007 <cloveryuno664@gmail.com>
2022-09-13 16:34:58 +0200

    _Print alphabet, betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet
  */
 
 void print_alphabet(void)
 {
-char c;  
-for ( c= 'a' ; c <= 'z' ; c++ )
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
 putchar(c);
 putchar('\n');
 }
