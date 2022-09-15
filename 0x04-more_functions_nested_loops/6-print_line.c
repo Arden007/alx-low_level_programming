77e76a76c02d7d73eb646ae087ef7e201e395f21
Arden007 <cloveryuno664@gmail.com>
2022-09-15 20:52:38 +0200

    straight line, betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,13 @@
 
 void print_line(int n)
 {
-    int ln;
-
-    if (n <= 0)
-        _putchar('\n');
-    else
-    {
-        for (ln = 1; ln <= n; ++ln)
-            _putchar('_');
-        _putchar('\n');
-    }
+int ln;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (ln = 1; ln <= n; ++ln)
+_putchar('_');
+_putchar('\n');
+}
 }
