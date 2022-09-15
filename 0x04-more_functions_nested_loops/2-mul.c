c3bdad1cd30d5fca560c8773b4b2ca60eeef34a1
Arden007 <cloveryuno664@gmail.com>
2022-09-15 19:16:20 +0200

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/*
+ * mul - multiples 2 integer numbers
+ *
+ * @a: input for integer
+ * 
+ * @b: input for integer
+ *
+ * Return: result of the sum
+ */
+
+int mul(int a, int b){
+    int sum;
+    sum = a * b ;
+    return (sum);
+}
