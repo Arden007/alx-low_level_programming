d5ed200aaa1ff30ca1b7b810d00a0ada597726bf
Arden007 <cloveryuno664@gmail.com>
2022-09-15 20:03:41 +0200

    Count 0-9, lets start from over, betty really now

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_numbers - print 0 - 9
  *             only using _putchar twice
@@ -10,11 +9,10 @@
 void print_numbers(void)
 {
 int c = 0;
-
 do
 {
 _putchar(c + 48);
 c++;
-} while (c >= 0 && c <= 9);
+}while (c >= 0 && c <= 9);
 _putchar('\n');
 }
