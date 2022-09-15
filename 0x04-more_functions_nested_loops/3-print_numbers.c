0e823dd9fee7cf358b07f048a4ff4e8844aa9013
Arden007 <cloveryuno664@gmail.com>
2022-09-15 19:26:37 +0200

    Count 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * print_numbers - prints numbers from 0 - 9
+ *
+ * Return: 0 for success
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+putchar(c);
+}
+putchar("\n");
+return (0);
+}
