b9351f8068f3fcf28c209f513c56656e7a9e297f
Arden007 <cloveryuno664@gmail.com>
2022-09-15 20:46:03 +0200

    repeat x10,

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,20 @@
 
 void more_numbers(void)
 {
-int i;
-char c;
-for (i = 0 ; i < 10 ; i++)
-{
-for (c = '0' ; c <= '14' ; c++)
-{
-if (c > '9')
-{
-_putchar((c / 10) + '48');
-_putchar((c % 10) + '48');
-}
-_putchar('\n');
-}
+    int num, row, count;
+
+    for (row = 1; row <= 10; ++row)
+    {
+        for (count = 0; count <= 14; ++count)
+        {
+            num = count;
+            if (count > 9)
+            {
+                _putchar(1 + 48);
+                num = count % 10;
+            }
+            _putchar(num + 48);
+        }
+        _putchar('\n');
+    }
 }
