221bc61589f1efa292b132ed2409c867801ca71b
Arden007 <cloveryuno664@gmail.com>
2022-09-15 20:43:46 +0200

    repeat x10, modules, that char

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,12 @@ int i;
 char c;
 for (i = 0 ; i < 10 ; i++)
 {
-for (c = 0 ; c <= 14 ; c++)
+for (c = '0' ; c <= '14' ; c++)
 {
 if (c > 9)
 {
-_putchar((c / 10) + 48);
-_putchar((c % 10) + 48);
+_putchar((c / 10) + '48');
+_putchar((c % 10) + '48');
 }
 _putchar('\n');
 }
