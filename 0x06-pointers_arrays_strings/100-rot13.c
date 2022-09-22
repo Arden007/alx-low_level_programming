74a915d3b2c0c4062c580f8f06381f8becfd92c3
Arden007 <cloveryuno664@gmail.com>
2022-09-22 19:28:28 +0200

    Encode rot13, rewrite logic

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,15 +6,19 @@
 
 char *rot13(char *s)
 {
-int i;
-char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-char storel[] = "nopqrstuvwxyzabcdefghijklm";
+int i, c;
+char storeh[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char storel[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 for (i = 0; s[i] != '\0'; i++)
 {
-if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+for (c = 0; storeh[c] != '\0'; c++)
 {
-s[i] = (s[i] - 65 > 25) ? storel[s[i] - 97] : storeh[s[i] - 65];
+if (s[i] == storeh[c])
+{
+s[i] = storel[c];
+break;
+}
 }
 }
 return (s);
