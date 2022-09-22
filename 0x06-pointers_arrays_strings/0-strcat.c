5c4b21eb1b252a1a73be34e06a7fb9ee9815332f
Arden007 <cloveryuno664@gmail.com>
2022-09-22 18:12:15 +0200

    concaternation part 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
  * _strcat - concatenates two strings by appending the source to the end destination string
  * 
@@ -8,13 +7,14 @@
  * 
  * Return: the pointer
  */
+
 char *_strcat(char *dest, char *src)
 {
 int d , s;
-/* first we need to determine the length of the string */
+
 for (d = 0 ; dest[d] ; d++);
 for (s = 0 ; src[s] ; s++)
-/* after we determine the length of both strings we can append the strings and to overwrite the null byte lets iterate dest*/
 dest[d++] = src[s];
-return dest;
+
+return (dest);
 }
