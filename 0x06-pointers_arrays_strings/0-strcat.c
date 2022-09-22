e56b8f5f3f6b566b3b357b31016bc99aa5ffead7
Arden007 <cloveryuno664@gmail.com>
2022-09-22 18:17:50 +0200

    concaternation part 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-    int c, c2;
+int c, c2;
 
-    c = 0;
-    while (dest[c])
-        c++;
+c = 0;
+while (dest[c])
+c++;
+for (c2 = 0; src[c2]; c2++)
+dest[c++] = src[c2];
 
-    for (c2 = 0; src[c2]; c2++)
-        dest[c++] = src[c2];
-
-    return (dest);
+return (dest);
 }
