552ef35981e746c574d8cabba25868ff26d84614
Arden007 <cloveryuno664@gmail.com>
2022-09-22 20:54:14 +0200

    putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,17 +6,18 @@
 void print_number(int n)
 {
 
-    if (n / 10 != 0)
+    if (n < 0)
     {
-        putchar(n % 10);
-        print_number((n - n % 10) / 10);
+        putchar('-');
+        n = -n;
     }
 
     if (n == 0)
         putchar('0');
 
     if (n / 10)
-        print_number(n / 10);
+        printnumber(n / 10);
 
     putchar(n % 10 + '0');
 }
+
