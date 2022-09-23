a7ea1ae2713f2767e45911d444e12813e803c610
Arden007 <cloveryuno664@gmail.com>
2022-09-23 13:45:30 +0200

    putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_number - Print an integer using only _putchar
  * @n: integer to print
@@ -16,7 +17,7 @@ void print_number(int n)
         putchar('0');
 
     if (n / 10)
-        printnumber(n / 10);
+        print_number(n / 10);
 
     putchar(n % 10 + '0');
 }
