6e1003ce79a1103b33fa875fcb2591865fcc9080
Arden007 <cloveryuno664@gmail.com>
2022-09-28 21:34:50 +0200

    fix compile, betty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,16 @@
  * _puts_recursion()- prints a string followed by a new line
  *
  *  @s: the string to be printed
- * 
  * Return: Always 0 (Success)
  */
 
 void _puts_recursion(char *s)
 {
-    if(*s != '\0')
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-        _putchar('\n');
+if(*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
