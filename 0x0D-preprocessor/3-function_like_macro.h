ea9eea780171d48ce05a58d7eeeb8052aea0fffe
Arden007 <cloveryuno664@gmail.com>
2022-10-10 15:08:04 +0200

    Preprocesser task complete

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _PI_H_
+#define _PI_H_
+
+#define ABS(D) (((D) >= 0) ? (D) : ((D) * (-1)))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _PI_H_
+#define _PI_H_
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
