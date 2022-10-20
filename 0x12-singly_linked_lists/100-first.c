e9b51fceac60cd7fa088de0989c15a6a44968234
Arden007 <cloveryuno664@gmail.com>
2022-10-20 22:57:23 +0200

    Link list advance

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  *
  * Return: nothing
  */
-void __attribute__ ((constructor))first()
+
+void __attribute__((constructor))first()
 {
 printf("You're beat! and yet, you must allow");
 printf(",\nI bore my house upon my back!\n");
