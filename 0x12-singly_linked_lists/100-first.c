a8d09aca9ee3ca6c4b1edfe028d89c379fec54f3
Arden007 <cloveryuno664@gmail.com>
2022-10-20 22:55:45 +0200

    Link list advance

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 /**
  * show_first - executes before the main function
- *@first- prints from init by the loader before the main
- 
+ *
  * Return: nothing
  */
 
-void __attribute__((constructor))first()
+void __attribute__((constructor)) first()
 {
 printf("You're beat! and yet, you must allow");
 printf(",\nI bore my house upon my back!\n");
