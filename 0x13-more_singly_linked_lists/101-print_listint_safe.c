0e0517d8bf6c5b60718463a596ab1be645919344
Arden007 <cloveryuno664@gmail.com>
2022-10-26 04:20:30 +0200

    linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print_listint_safe - define function
+ * @head: pointer to the starting node of the
+ * listint_t linked list
+ * Return: 0 on success, -1 on failure
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+head++;
+return (0);
+}
